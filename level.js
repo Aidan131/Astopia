fb67d10a7ddb07e6135cce99ca9866612a5a8724
Aidan131 <114149945+Aidan131@users.noreply.github.com>
2025-08-09 01:41:18 +0800

    Delete level.js

--- level.js
+++
@@ -1,9 +0,0 @@
-function level1(){
-    window.location.href = "inner.html"
-}
-function level2(){
-    window.location.href = "inner.html"
-}
-function level3(){
-    window.location.href = "inner.html"
-}
