083286e0564a51b67239a2346cd24eab29698b65
Aidan131 <114149945+Aidan131@users.noreply.github.com>
2025-09-06 20:51:51 +0800

    Update next.js

--- static1/next.js
+++ static1/next.js
@@ -4,16 +4,18 @@ let current = 1;
 function next() {
     if (current!=7){
         items.forEach((item, index) => {
-            item.style.transform = `translateX(calc((-500px - 4vw) * ${current}))`;
-        });
+            item.style.transform = `translateX( calc(-32vw * ${current}))`;
+        }); 
         current = current + 1;
     }
 }
 
+
+
 function prev() {
     if (current!=1){
         items.forEach((item, index) => {
-            item.style.transform =  `translateX(calc((-500px - 4vw) * ${current-2}))`;
+            item.style.transform =  `translateX( calc(-32vw * ${current-2}))`;
         });
         current = current -1;
     };
