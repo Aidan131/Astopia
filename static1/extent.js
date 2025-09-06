e08a99166763f168c70f31fd3c20136b59ad08dc
Aidan131 <114149945+Aidan131@users.noreply.github.com>
2025-09-06 18:51:36 +0800

    Add files via upload

---
+++ static1/extent.js
@@ -0,0 +1,24 @@
+const element = document.getElementById('myElement');
+
+
+function toggleCard(card) {
+            const allCards = document.querySelectorAll('.card');
+            const wasActive = card.classList.contains('active');
+            
+            
+            allCards.forEach(c => {
+                c.classList.remove('active');
+                c.style.display = 'none';
+            });
+            
+            if (!wasActive) {
+                card.classList.add('active');
+                card.style.display = 'block';
+                card.style.width = '60%';
+            } else {
+                allCards.forEach(c => {
+                    c.style.display = 'block';
+                    c.style.width = ''; 
+                });
+            }
+        }
