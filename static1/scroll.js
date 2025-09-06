81bdcdd8b62df526c4f9148803b33e13770d9045
Aidan131 <114149945+Aidan131@users.noreply.github.com>
2025-09-06 11:02:06 +0800

    Update scroll.js

--- static1/scroll.js
+++ static1/scroll.js
@@ -2,15 +2,11 @@ const header = document.querySelector('header');
 const logo = document.querySelector('.logo');
 const toggle_wrapper = document.querySelector('.toggle-wrapper');
 
-// Function to check scroll position and toggle class
 function handleScroll() {
-  // Get the current vertical scroll position
   const scrollY = window.scrollY;
 
-  // Define a trigger point (e.g., 50 pixels)
   const triggerPoint = 100;
 
-  // Check if scrolled past the trigger point
   if (scrollY > triggerPoint) {
     header.classList.add('scrolled');
     logo.classList.add('scrolled')
@@ -22,8 +18,6 @@ function handleScroll() {
   }
 }
 
-// Listen for the scroll event and run the handleScroll function
 window.addEventListener('scroll', handleScroll);
 
-// Optional: Run it once on page load in case the page is reloaded while scrolled down
 handleScroll();
