410894f84d07d240ab65be67115dd0eb663583d7
Aidan131 <114149945+Aidan131@users.noreply.github.com>
2025-09-08 18:18:50 +0800

    Update scroll.js

--- static1/scroll.js
+++ static1/scroll.js
@@ -2,22 +2,22 @@ const header = document.querySelector('header');
 const logo = document.querySelector('.logo');
 const toggle_wrapper = document.querySelector('.toggle-wrapper');
 
+
 function handleScroll() {
   const scrollY = window.scrollY;
 
   const triggerPoint = 100;
 
-  if (scrollY > triggerPoint) {
-    header.classList.add('scrolled');
-    logo.classList.add('scrolled')
-    toggle_wrapper.classList.add('scrolled')
+  if (scrollY > triggerPoint && window.innerWidth > 600) {
+    header.classList.add('scrolled'); 
+    logo.classList.add('scrolled');
+    toggle_wrapper.classList.add('scrolled');
   } else {
     header.classList.remove('scrolled');
-    logo.classList.remove('scrolled')
-    toggle_wrapper.classList.remove('scrolled')
+    logo.classList.remove('scrolled');
+    toggle_wrapper.classList.remove('scrolled');
   }
 }
 
 window.addEventListener('scroll', handleScroll);
-
 handleScroll();
