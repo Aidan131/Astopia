af09eee9be896484d86c85a408eb12b0c331c7d9
Aidan131 <114149945+Aidan131@users.noreply.github.com>
2025-09-16 02:24:39 +0800

    Update n.js

--- static1/n.js
+++ static1/n.js
@@ -3,23 +3,45 @@ const word = document.querySelector('.word-last');
 let current = 1;
 
 function next() {
-    if (current!=7){
-        items.forEach((item, index) => {
-            item.style.transform = `translateX( calc(-32vw * ${current}))`;
-        }); 
-        word.style.transform = `translateX( calc(-32vw * ${current}))`;
-        current = current + 1;
+    if (window.innerWidth > 600){
+        if (current!=7){
+            items.forEach((item, index) => {
+                item.style.transform = `translateX( calc(-32vw * ${current}))`;
+            }); 
+            word.style.transform = `translateX( calc(-32vw * ${current}))`;
+            current = current + 1;
+        }
+        console.log(window.innerWidth);
+    } else{
+        if (current!=7){
+            items.forEach((item, index) => {
+                item.style.transform = `translateX(calc(-92vw * ${current}))`;
+            }); 
+            word.style.transform = `translateX(calc(-92vw * ${current}))`;
+            current = current + 1;
+        };
+        console.log(window.innerWidth);
     }
 }
 
 
 
 function prev() {
-    if (current!=1){
-        items.forEach((item, index) => {
-            item.style.transform =  `translateX( calc(-32vw * ${current-2}))`;
-        });
-        word.style.transform =  `translateX( calc(-32vw * ${current-2}))`;
-        current = current -1;
-    };
-}
+    if (window.innerWidth > 600){
+        if (current!=1){
+            items.forEach((item, index) => {
+                item.style.transform =  `translateX( calc(-32vw * ${current-2}))`;
+            });
+            word.style.transform =  `translateX( calc(-32vw * ${current-2}))`;
+            current = current -1;
+        };
+    } else{
+        if (current!=1){
+            items.forEach((item, index) => {
+                item.style.transform =  `translateX( calc(-92vw * ${current-2}))`;
+            });
+            word.style.transform =  `translateX( calc(-92vw * ${current-2}))`;
+            current = current -1;
+        };
+    }
+}   
